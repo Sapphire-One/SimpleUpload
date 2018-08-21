e9944ef69ee6e3f3033095c8fb047c90a051fb48
Martin Hyland <martin-hyland+Developer@warriewood.net.au>
2018-08-22 06:05:30 +1000

    Add bootstrap

--- templates/form.php
+++ templates/form.php
@@ -1,17 +1,53 @@
 <!DOCTYPE html>
-<html>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<body>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Martin Hyland">
 
-<form action="upload.php" method="post" enctype="multipart/form-data">
-    Select image to upload:
-    <input type="file" name="fileToUpload" id="fileToUpload">
-    <br>
-    Enter your upload password:
-    <input type="password" name="upload-secret">
-    <br>
-    <input type="submit" value="Upload Image" name="submit">
-</form>
+    <title>SimpleUpload</title>
 
-</body>
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <link href="./css/stylesheet.css" rel="stylesheet">
+  </head>
+  <body>
+    <div class="site-wrapper">
+      <div class="site-wrapper-inner">
+        <div class="cover-container">
+          <div class="masthead clearfix">
+            <div class="inner">
+              <h3 class="masthead-brand">Image uploader</h3>
+            </div>
+          </div>
+          <div class="inner cover">
+            <form action="upload.php" method="post" enctype="multipart/form-data">
+                Select image to upload:
+                <input type="file" name="fileToUpload" id="fileToUpload">
+                <br>
+                Enter your upload password:
+                <input type="password" name="upload-secret">
+                <br>
+                <input type="submit" value="Upload Image" name="submit">
+            </form>
+            </div>
+          <div class="mastfoot">
+            <div class="inner">
+              <p>SimpleUplader</p>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script src="./js/js.js"></script>
+  </body>
 </html>
