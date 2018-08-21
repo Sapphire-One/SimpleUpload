fa8e053da825dddc1669ccb280b301df810ec6dc
Martin Hyland <martin-hyland+Developer@warriewood.net.au>
2018-08-22 01:18:44 +1000

    Remove personal server settings

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 // Set save directory for your uploads!
-$target_dir = "/home/oniivjnw/public_html/images/anilist/uploads/";
+$target_dir = "/home/public_html/images/uploads/";
 // Set your upload password to stop your upload system from being abused!
 $upload_password = "Password";
 // Set protocall, this should be either "https://" or "http://" and it depends on your server configuration
